6963faf54f815793c349a0dd4bc015ab8f61aa6e
Angie Amado <angieamado76@gmail.com>
2025-03-31 20:26:39 +0200

    Jugando Con Arrays Ej 1 y 2

--- src/ejercicios/interfaces/funcionales/JugandoConArrays.java
+++ src/ejercicios/interfaces/funcionales/JugandoConArrays.java
@@ -1,17 +1,35 @@
 package ejercicios.interfaces.funcionales;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class JugandoConArrays {
 	
-	public Integer [] transformar (Integer [] arrayInteger, Function<Integer, Integer> funcion) {
+	public static Integer [] transformar (Integer [] arrayInteger, Function<Integer, Integer> funcion) {
 		
-		Integer [] integerFinal;
+		//Agrego el static para trabajarlo sin instanciar 
+		
+		Integer [] integerFinal =  new Integer[arrayInteger.length]; //Se debe crear el array final con la dimensión que tendrá al final
 		
 		
-		
-		
-		return null;
+		for (int i = 0; i < arrayInteger.length; i++) { 
+            integerFinal[i] = funcion.apply(arrayInteger[i]); //Se usa el índice para facilitar su almacenamiento
+        }
+				
+		return integerFinal;
 	}
+	
+	
+	public static void procesar (Integer [] arrayInteger, Consumer<Integer> funcion) {
+		
+		for (int i = 0; i < arrayInteger.length; i++) { 
+            funcion.accept(arrayInteger[i]);
+        }
+		
+		
+		
+	}
+	
+	
 	
 }
--- src/ejercicios/interfaces/funcionales/Test.java
+++ src/ejercicios/interfaces/funcionales/Test.java
@@ -3,12 +3,32 @@ package ejercicios.interfaces.funcionales;
 public class Test {
 	
 	
-	
-	
 	public static void main(String[] args) {
 		
-
-
+		System.out.println("PROBANDO TRANSFORMAR CON FUNCTION");
+		Integer [] prueba1 = JugandoConArrays.transformar(new Integer [] {1, 2, 3, 4, 5} , x -> x + 1);
+		
+		for (Integer num : prueba1) {
+			System.out.println(num);
+		
+		}
+		
+		Integer [] prueba2 = JugandoConArrays.transformar(new Integer [] {1, 2, 3, 4, 5} , x -> x - 1);
+		
+		for (Integer num : prueba2) {
+			System.out.println(num);
+		
+		}
+		
+		System.out.println("PROBANDO PROCESAR CON CONSUMER");
+		
+		JugandoConArrays.procesar(new Integer [] {1, 2, 3, 4, 5} , x -> System.out.println(x));
+		
+		
+		
+		
+		
+				
 	}
 
 }
